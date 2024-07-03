7da007bbd3e995c6d14bb69c87a149914d44e340
Ricky de Laveaga <git@rdela.com>
2024-07-03 14:37:02 -0700

    Donut sprinkles
    - Change colors so more obvious when looking at Donut experimental add-ons
      vs Tugboat
    
    - Update repo info to hopefully avoid more confusion than necessary
    
    - Add demo of first experimental add-on component, `emoji-link`, a hybrid of
      `favicon-link` and `accessible-emoji`

--- _data/metadata.cjs
+++ _data/metadata.cjs
@@ -2,8 +2,8 @@ const pkg = require("../package.json");
 
 module.exports = {
 	// Feel free to override these, they are pulling from package.json for default values.
-	title: "Tugboat" || pkg.name,
+	title: "Donut" || pkg.name,
 	description: "" || pkg.description,
 	language: "en",
-	repository: "https://github.com/11ty/tugboat/blob/main/",
+	repository: "https://github.com/rdela/tugboat/blob/donut/",
 };
